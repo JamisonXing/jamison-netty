4de9b844eebd3d62ca2f2f39f745a8aee1a81451
jamison <2810947907@qq.com>
2023-03-13 17:17:28 +0800

    TestFilesWalkFileTree

---
+++ netty-demo/src/test/java/cn/jamison/netty/c1/TestFilesWalkFileTree.java
@@ -0,0 +1,45 @@
+package cn.jamison.netty.c1;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TestFilesWalkFileTree {
+    public static void main(String[] args) throws IOException {
+        AtomicInteger jarCount = new AtomicInteger();
+        Files.walkFileTree(Paths.get(" "), new SimpleFileVisitor<Path>(){
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if(file.toString().endsWith(".jar")) {
+                    System.out.println(file);
+                    jarCount.incrementAndGet();
+                }
+                return super.visitFile(file, attrs);
+            }
+        });
+    }
+
+    public static void m1(String[] args) throws IOException {
+        AtomicInteger dirCount = new AtomicInteger();
+        AtomicInteger fileCount = new AtomicInteger();
+        Files.walkFileTree(Paths.get(" "), new SimpleFileVisitor<Path>(){
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                System.out.println("===>" + dir);
+                dirCount.incrementAndGet();
+                return super.preVisitDirectory(dir, attrs);
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                System.out.println(file);
+                fileCount.incrementAndGet();
+                return super.visitFile(file, attrs);
+            }
+        });
+
+        System.out.println("dir count" + dirCount);
+        System.out.println("file count" + fileCount);
+    }
+}
